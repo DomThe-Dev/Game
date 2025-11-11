20cdf59c4db787404a0a0365a8990bbb93c5179f
DomThe Dev <dominykas.jucys1@gmail.com>
2025-11-11 01:43:40 +0000

    Made better singleton for the ResourceManager. SceneFactory was supposed to exist already,

--- Game/src/managers/ResourceManager.h
+++ Game/src/managers/ResourceManager.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+/// <summary>
+/// Loads and returns the assets from file paths, and stores them in a map. Singleton.
+/// </summary>
 class ResourceManager
 {
 public:
-	ResourceManager() = default;
-private:
+	static ResourceManager& GetInstance();
 
+	/// <summary>
+	/// Load will first try to get the asset by ID, returning the asset if it exists.
+	/// If it does not exist, it will load the asset from the file path, and store it in a map.
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="file_path"></param>
+	/// <returns></returns>
+	bool Load(std::string id, std::string file_path);
+	void Get(std::string id); // Gets a certain asset by ID
+private:
+	ResourceManager(); // Private constructor for singleton pattern
+	ResourceManager(ResourceManager const&); // Stop copies
+	ResourceManager operator=(ResourceManager const&); // Stop copies
 };
