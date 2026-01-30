4a002b29352f4954f37e9da7aac182d87a11c461
DomThe_Dev <dominykas.jucys1@gmail.com>
2026-01-30 02:37:39 +0000

    Further work on the ResourceManager. Core functions added, design of the class is being worked on, alongside how it should function, etc.

--- Game/src/managers/ResourceManager.cpp
+++ Game/src/managers/ResourceManager.cpp
@@ -1,1 +1,52 @@
 #include "ResourceManager.h"
+
+void ResourceManager::LoadTexture(const std::string& id, const std::string& file_path)
+{
+	// If texture ID is already in use
+	if (textures_.find(id) != textures_.end())
+	{
+		spdlog::error("Texture ID: {} is already in use.", id);
+		return;
+	}
+
+	sf::Texture texture;
+	// If cannot load the file
+	if (!texture.loadFromFile(file_path))
+	{
+		spdlog::error("Could not load texture with ID: {}, at file location: {}.", id, file_path);
+		return;
+	}
+
+	// No errors, so continue
+	textures_[id] = std::move(texture);
+}
+
+const sf::Texture* ResourceManager::GetTexture(const std::string& id) const
+{
+	// Using the std::optional thing because maybe the reference doesn't exist, and something needs to be handed back.
+	// This does mean that 
+
+	auto it = textures_.find(id);
+
+	// If can't find the texture
+	if (it == textures_.end())
+	{
+		spdlog::error("Cannot find the texture with ID: {}", id);
+		return nullptr; // Return with an error texture maybe?
+	}
+	
+	// Has found it
+	return &it->second;
+}
+
+void ResourceManager::UnloadTexture(const std::string& id)
+{
+	auto it = textures_.find(id);
+
+	if (it == textures_.end())
+	{
+		spdlog::warn("Could not unload texture with ID: {}", id); // No harm no foul?
+		return;
+	}
+	textures_.erase(it);
+}
--- Game/src/managers/ResourceManager.h
+++ Game/src/managers/ResourceManager.h
@@ -1,29 +1,52 @@
 #pragma once
 #include <string>
 #include <unordered_map>
-//#include <memory>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <json.hpp>
+
+#include "../utils/Common.h"
 
 /*
-* Create a singleton resource manager class
-* Have a resource class
-* Resource class is inherited by different asset types (e.g. Texture, Sound, Model)
-*	Reource class has a path to the file
-*	Child class has the actual data for it, e.g texture, timings, volume, etc
+* Could remove singleton and reuse the class:
+*	Core resources for the menu, menu font, etc
+*	Scene resources for the scene coming up, meaning that everything will be deleted when the scene is gone
 */
 
 /// <summary>
 /// Loads and returns the assets from file paths, and stores them in a map. Singleton.
 /// </summary>
-template<typename Resource>
 class ResourceManager
 {
 public:
+	/// <summary>
+	/// Returns instance of the singleton class.
+	/// </summary>
+	/// <returns></returns>
 	static ResourceManager& GetInstance()
 	{
 		static ResourceManager instance;
 		return instance;
 	}
 
+	// Texture
+	void LoadTexture(const std::string& id, const std::string& file_path);
+	const sf::Texture* GetTexture(const std::string& id) const;
+	void UnloadTexture(const std::string& id);
+
+	// Audio
+	void LoadAudio(const std::string& id, const std::string& file_path);
+	const sf::AudioResource& GetAudio(const std::string& id) const;
+	void UnloadAudio(const std::string& id);
+
+	// Font
+	void LoadFont(const std::string& id, const std::string& file_path);
+	const sf::Font GetFont(const std::string& id) const;
+	void UnloadFont(const std::string& id);
+	
+	// Cleanup
+	void UnloadAll();
+	
 private:
 	// Singleton class.
 	ResourceManager() = default; // Private constructor.
@@ -32,5 +55,8 @@ private:
 	ResourceManager(ResourceManager&&) = delete; // Delete move constructor.
 	ResourceManager& operator=(ResourceManager&&) = delete; // Delete move assignment operator.
 
-	
+	// Maps of the resources
+	std::unordered_map<std::string, sf::Texture> textures_;
+	std::unordered_map<std::string, sf::AudioResource> audios_;
+	std::unordered_map<std::string, sf::Font> fonts_;
 };
